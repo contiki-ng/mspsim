d132430db9ad854032c9440f8675477fc42ea0ab
nifi <nifi@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2007-12-06 16:40:33 +0000

    highlighting c source viewer
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@39 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

---
+++ se/sics/mspsim/extutil/highlight/HighlightSourceViewer.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: $
+ *
+ * -----------------------------------------------------------------
+ *
+ * HighlightSourceViewer
+ *
+ * Authors : Adam Dunkels, Joakim Eriksson, Niclas Finne
+ * Created : 6 dec 2007
+ * Updated : $Date: 6 dec 2007 $
+ *           $Revision: 1.0 $
+ */
+
+package se.sics.mspsim.extutil.highlight;
+
+import java.awt.Container;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import se.sics.mspsim.util.SourceViewer;
+
+/**
+ *
+ */
+public class HighlightSourceViewer implements SourceViewer {
+
+  private JFrame window;
+  private SyntaxHighlighter highlighter;
+  
+  public HighlightSourceViewer() {
+    //
+  }
+
+  private void setup() {
+    if (window == null) {
+      window = new JFrame("Source Viewer");
+      window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+      Scanner scanner = new CScanner();
+      highlighter = new SyntaxHighlighter(24, 80, scanner);
+      highlighter.setBorder(new LineNumberedBorder(LineNumberedBorder.LEFT_SIDE, LineNumberedBorder.RIGHT_JUSTIFY));
+      JScrollPane scroller = new JScrollPane(highlighter);
+      scroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+      Container pane = window.getContentPane();
+      pane.add(scroller);
+      window.pack();
+    }
+  }
+
+  public boolean isVisible() {
+    return window != null && window.isVisible();
+  }
+
+  public void setVisible(boolean isVisible) {
+    setup();
+    window.setVisible(isVisible);
+  }
+
+  public void viewFile(final String file) {
+    setup();
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          FileReader reader = new FileReader(file);
+          try {
+            highlighter.read(reader, null);
+            // Workaround for bug 4782232 in Java 1.4
+            highlighter.setCaretPosition(1);
+            highlighter.setCaretPosition(0);
+          } finally {
+            reader.close();
+          }
+        } catch (IOException err) {
+          err.printStackTrace();
+          JOptionPane.showMessageDialog(window, "Failed to read the file '" + file + '\'', "Could not read file", JOptionPane.ERROR_MESSAGE);
+        }
+      }
+    });
+  }
+
+  public void viewLine(final int line) {
+    if (highlighter != null) {
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          if (line >= 0 && line < highlighter.getLineCount()) {
+            highlighter.setCaretPosition(highlighter.getLineStartOffset(line));
+          }
+        }
+      });
+    }
+  }
+
+  public static void main(String[] args) {
+    HighlightSourceViewer sv = new HighlightSourceViewer();
+    sv.setVisible(true);
+    sv.viewFile(args[0]);
+  }
+}
---
+++ se/sics/mspsim/extutil/highlight/SyntaxHighlighter.java
@@ -0,0 +1,239 @@
+package se.sics.mspsim.extutil.highlight;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.event.*;
+import java.io.*;
+
+// Public domain, no restrictions, Ian Holyer, University of Bristol.
+
+/**
+ * Display text with syntax highlighting. Highlighting is done with full
+ * accuracy, using a given language scanner. Large amounts of re-highlighting
+ * are done in small bursts to make sure the user interface doesn't freeze.
+ */
+public class SyntaxHighlighter extends JTextPane implements DocumentListener, TokenTypes {
+
+  
+  private static final long serialVersionUID = -1801145479677890566L;
+
+  private StyledDocument doc;
+  private Scanner scanner;
+  private int rows, columns;
+
+  /**
+   * Create a graphics component which displays text with syntax highlighting.
+   * Provide a width and height, in characters, and a language scanner.
+   */
+  public SyntaxHighlighter(int rows, int columns, Scanner scanner) {
+    super(new DefaultStyledDocument());
+    doc = (StyledDocument) getDocument();
+    this.rows = rows;
+    this.columns = columns;
+    this.scanner = scanner;
+    doc.addDocumentListener(this);
+    Font font = new Font("Monospaced", Font.PLAIN, getFont().getSize());
+    changeFont(font);
+    initStyles();
+  }
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public int getRows() {
+    return rows;
+  }
+
+  /**
+   * Change the component's font, and change the size of the component to match.
+   */
+  public void changeFont(Font font) {
+    int borderOfJTextPane = 3;
+    setFont(font);
+    FontMetrics metrics = getFontMetrics(font);
+    int paneWidth = columns * metrics.charWidth('m') + 2 * borderOfJTextPane;
+    int paneHeight = rows * metrics.getHeight() + 2 * borderOfJTextPane;
+    Dimension size = new Dimension(paneWidth, paneHeight);
+    setMinimumSize(size);
+    setPreferredSize(size);
+    invalidate();
+  }
+
+  /**
+   * Read new text into the component from a <code>Reader</code>. Overrides
+   * <code>read</code> in <code>JTextComponent</code> in order to highlight
+   * the new text.
+   */
+  public void read(Reader in, Object desc) throws IOException {
+    int oldLength = getDocument().getLength();
+    doc.removeDocumentListener(this);
+    super.read(in, desc);
+    doc = (StyledDocument) getDocument();
+    doc.addDocumentListener(this);
+    int newLength = getDocument().getLength();
+    firstRehighlightToken = scanner.change(0, oldLength, newLength);
+    repaint();
+  }
+
+  // An array of styles, indexed by token type. Default styles are set up,
+  // which can be used for any languages.
+
+  private Style[] styles;
+
+  private void initStyles() {
+    styles = new Style[typeNames.length];
+    changeStyle(UNRECOGNIZED, Color.red);
+    changeStyle(WHITESPACE, Color.black);
+    changeStyle(WORD, Color.black);
+    changeStyle(NUMBER, Color.orange.darker());
+    changeStyle(PUNCTUATION, Color.orange.darker());
+    changeStyle(COMMENT, Color.green.darker());
+    changeStyle(START_COMMENT, Color.green.darker());
+    changeStyle(MID_COMMENT, Color.green.darker());
+    changeStyle(END_COMMENT, Color.green.darker());
+    changeStyle(TAG, Color.blue, Font.BOLD);
+    changeStyle(END_TAG, Color.blue, Font.BOLD);
+    changeStyle(KEYWORD, Color.blue);
+    changeStyle(KEYWORD2, Color.blue);
+    changeStyle(IDENTIFIER, Color.black);
+    changeStyle(LITERAL, Color.magenta);
+    changeStyle(STRING, Color.magenta);
+    changeStyle(CHARACTER, Color.magenta);
+    changeStyle(OPERATOR, Color.black, Font.BOLD);
+    changeStyle(BRACKET, Color.orange.darker());
+    changeStyle(SEPARATOR, Color.orange.darker());
+    changeStyle(URL, Color.blue.darker());
+
+    for (int i = 0; i < styles.length; i++) {
+      if (styles[i] == null) {
+        styles[i] = styles[WHITESPACE];
+      }
+    }
+  }
+
+  /**
+   * Change the style of a particular type of token.
+   */
+  public void changeStyle(int type, Color color) {
+    Style style = addStyle(typeNames[type], null);
+    StyleConstants.setForeground(style, color);
+    styles[type] = style;
+  }
+
+  /**
+   * Change the style of a particular type of token, including adding bold or
+   * italic using a third argument of <code>Font.BOLD</code> or
+   * <code>Font.ITALIC</code> or the bitwise union
+   * <code>Font.BOLD|Font.ITALIC</code>.
+   */
+  public void changeStyle(int type, Color color, int fontStyle) {
+    Style style = addStyle(typeNames[type], null);
+    StyleConstants.setForeground(style, color);
+    if ((fontStyle & Font.BOLD) != 0)
+      StyleConstants.setBold(style, true);
+    if ((fontStyle & Font.ITALIC) != 0)
+      StyleConstants.setItalic(style, true);
+    styles[type] = style;
+  }
+
+  public int getLineCount() {
+    return getDocument().getDefaultRootElement().getElementCount();
+  }
+
+  public int getLineStartOffset(int line) {
+    Element root = getDocument().getDefaultRootElement();
+    if (line < 0 || line >= root.getElementCount()) {
+      throw new IndexOutOfBoundsException("illegal line");
+    }
+    return root.getElement(line).getStartOffset();
+  }
+
+  public int getLineEndOffset(int line) {
+    Element root = getDocument().getDefaultRootElement();
+    if (line < 0 || line >= root.getElementCount()) {
+      throw new IndexOutOfBoundsException("illegal line");
+    }
+    return root.getElement(line).getEndOffset();
+  }
+
+  /**
+   * <font style='color:gray;'>Ignore this method. Responds to the underlying
+   * document changes by re-highlighting.</font>
+   */
+  public void insertUpdate(DocumentEvent e) {
+    int offset = e.getOffset();
+    int length = e.getLength();
+    firstRehighlightToken = scanner.change(offset, 0, length);
+    repaint();
+  }
+
+  /**
+   * <font style='color:gray;'>Ignore this method. Responds to the underlying
+   * document changes by re-highlighting.</font>
+   */
+  public void removeUpdate(DocumentEvent e) {
+    int offset = e.getOffset();
+    int length = e.getLength();
+    firstRehighlightToken = scanner.change(offset, length, 0);
+    repaint();
+  }
+
+  /**
+   * <font style='color:gray;'>Ignore this method. Responds to the underlying
+   * document changes by re-highlighting.</font>
+   */
+  public void changedUpdate(DocumentEvent e) {
+    // Do nothing.
+  }
+
+  // Scan a small portion of the document. If more is needed, call repaint()
+  // so the GUI gets a go and doesn't freeze, but calls this again later.
+
+  private Segment text = new Segment();
+
+  private int firstRehighlightToken;
+
+  private int smallAmount = 100;
+
+  /**
+   * <font style='color:gray;'>Ignore this method. Carries out a small amount of
+   * re-highlighting for each call to <code>repaint</code>.</font>
+   */
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    int offset = scanner.position();
+    if (offset < 0)
+      return;
+
+    int tokensToRedo = 0;
+    int amount = smallAmount;
+    while (tokensToRedo == 0 && offset >= 0) {
+      int length = doc.getLength() - offset;
+      if (length > amount) {
+        length = amount;
+      }
+      try {
+        doc.getText(offset, length, text);
+      } catch (BadLocationException e) {
+        return;
+      }
+      tokensToRedo = scanner.scan(text.array, text.offset, text.count);
+      offset = scanner.position();
+      amount = 2 * amount;
+    }
+    for (int i = 0; i < tokensToRedo; i++) {
+      Token t = scanner.getToken(firstRehighlightToken + i);
+      int length = t.symbol.name.length();
+      int type = t.symbol.type;
+      if (type < 0) {
+        type = UNRECOGNIZED;
+      }
+      doc.setCharacterAttributes(t.position, length, styles[type], false);
+    }
+    firstRehighlightToken += tokensToRedo;
+    if (offset >= 0) {
+      repaint(2);
+    }
+  }
+}
