4d3cb1aaf334d26de90af2556a17f577ebbfe0ae
fros4943 <fros4943@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2008-04-01 08:18:03 +0000

    separated m24p80 from external files
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@218 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

---
+++ se/sics/mspsim/chip/FileM25P80.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of MSPSim.
+ *
+ * $Id$
+ *
+ * -----------------------------------------------------------------
+ *
+ * ExtFlash
+ *
+ * Author  : Joakim Eriksson, Fredrik Osterlind
+ * Created : Sun Oct 21 22:00:00 2007
+ * Updated : $Date$
+ *           $Revision$
+ */
+
+package se.sics.mspsim.chip;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import se.sics.mspsim.core.*;
+
+public class FileM25P80 extends M25P80 {
+
+  private RandomAccessFile file;
+
+  public FileM25P80(MSP430Core cpu, String filename) {
+    super(cpu);
+    if (filename == null) {
+      filename = "flash.bin";
+    }
+    // Open flash file for R/W
+    try {
+      file = new RandomAccessFile(filename, "rw");
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return;
+    }
+    // Set size of flash
+    try {
+      file.setLength(1024 * 1024);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void seek(long pos) throws IOException {
+    file.seek(pos);
+  }
+
+  public int readFully(byte[] b) throws IOException {
+    return file.read(b);
+  }
+
+  public void write(byte[] b) throws IOException {
+    file.write(b);
+  }
+
+} // ExtFlash
