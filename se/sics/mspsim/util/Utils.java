7e99290d16f8f013ec6167d67e3685f71757a3d7
nifi <nifi@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2008-05-07 16:16:15 +0000

    added add/remove util methods for arrays
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@269 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

--- se/sics/mspsim/util/Utils.java
+++ se/sics/mspsim/util/Utils.java
@@ -75,8 +75,41 @@ public class Utils {
     return s;
   }
 
-  public static void main(String[] args) {
-    System.out.println("Hex 47 = " + hex8(47));
+  public static Object[] add(Class<?> componentType, Object[] array, Object value) {
+    Object[] tmp;
+    if (array == null) {
+      tmp = (Object[]) java.lang.reflect.Array.newInstance(componentType, 1);
+    } else {
+      tmp = (Object[]) java.lang.reflect.Array.newInstance(componentType, array.length + 1);
+      System.arraycopy(array, 0, tmp, 0, array.length);
+    }
+    tmp[tmp.length - 1] = value;
+    return tmp;
   }
+
+  public static Object[] remove(Object[] array, Object value) {
+    if (array != null) {
+      for (int index = 0, n = array.length; index < n; index++) {
+        if (value.equals(array[index])) {
+          if (n == 1) {
+            return null;
+          }
+          Object[] tmp = (Object[]) java.lang.reflect.Array.newInstance(array.getClass().getComponentType(), array.length - 1);
+          if (index > 0) {
+            System.arraycopy(array, 0, tmp, 0, index);
+          }
+          if (index < tmp.length) {
+            System.arraycopy(array, index + 1, tmp, index, tmp.length - index);
+          }
+          return tmp;
+        }
+      }
+    }
+    return array;
+  }
+
+//  public static void main(String[] args) {
+//    System.out.println("Hex 47 = " + hex8(47));
+//  }
 
 }
