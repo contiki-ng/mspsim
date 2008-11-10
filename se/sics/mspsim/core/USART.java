29cf9a84e7e4c8033447ae9e66bc4fc439ffe07d
nifi <nifi@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2008-11-10 10:31:21 +0000

    removed debug output
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@420 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

--- se/sics/mspsim/core/USART.java
+++ se/sics/mspsim/core/USART.java
@@ -292,7 +292,7 @@ public class USART extends IOUnit {
     if (baudRate == 0) baudRate = 1;
     // Is this correct??? Is it the DCO or smclkFRQ we should have here???
     tickPerByte = (8 * cpu.smclkFrq) / baudRate;
-    if (DEBUG || true) {
+    if (DEBUG) {
       System.out.println(getName() +  " Ticks per byte: " + tickPerByte);
     }
   }
