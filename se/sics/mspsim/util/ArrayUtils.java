6bba55ab7a89f81504a796380867d7f1229317e1
joxe <joxe@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2009-05-26 12:24:25 +0000

    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@540 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

---
+++ se/sics/mspsim/util/ArrayUtils.java
@@ -0,0 +1,38 @@
+package se.sics.mspsim.util;
+
+public class ArrayUtils {
+
+  public static Object[] add(Class componentType, Object[] array, Object value) {
+    Object[] tmp;
+    if (array == null) {
+      tmp = (Object[]) java.lang.reflect.Array.newInstance(componentType, 1);
+    } else {
+      tmp = (Object[]) java.lang.reflect.Array.newInstance(componentType, array.length + 1);
+      System.arraycopy(array, 0, tmp, 0, array.length);
+    }
+    tmp[tmp.length - 1] = value;
+    return tmp;
+  }
+
+  public static Object[] remove(Object[] array, Object value) {
+    if (array != null) {
+      for (int index = 0, n = array.length; index < n; index++) {
+        if (value.equals(array[index])) {
+          if (n == 1) {
+            return null;
+          }
+          Object[] tmp = (Object[]) java.lang.reflect.Array.newInstance(array.getClass().getComponentType(), array.length - 1);
+          if (index > 0) {
+            System.arraycopy(array, 0, tmp, 0, index);
+          }
+          if (index < tmp.length) {
+            System.arraycopy(array, index + 1, tmp, index, tmp.length - index);
+          }
+          return tmp;
+        }
+      }
+    }
+    return array;
+  }
+
+}
