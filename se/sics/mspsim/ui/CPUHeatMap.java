a3e6c70f66bd2aee2359219f5eecbde4632ad5b4
joxe <joxe@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2010-06-22 09:45:16 +0000

    added missing class
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@708 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

---
+++ se/sics/mspsim/ui/CPUHeatMap.java
@@ -0,0 +1,116 @@
+package se.sics.mspsim.ui;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import se.sics.mspsim.core.CPUMonitor;
+import se.sics.mspsim.core.MSP430Core;
+
+public class CPUHeatMap extends JComponent implements CPUMonitor {
+
+    private Timer ticker;
+    
+    private JFrame window;
+    private BufferedImage heatmap;
+    private int[] heatR = new int[MSP430Core.MAX_MEM];
+    private int[] heatW = new int[MSP430Core.MAX_MEM];
+    private int[] heatE = new int[MSP430Core.MAX_MEM];
+    private int heatMax = 0;
+    private int mode = 1;
+    
+    public CPUHeatMap() {
+        window = new JFrame("CPU Heat Map");
+        heatmap = new BufferedImage(128, 512, BufferedImage.TYPE_INT_RGB);
+        window.setBounds(100, 100, 140, 530);
+        window.setVisible(true);
+        window.add(this);
+        
+        ticker = new Timer(50, new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                repaint();
+            }
+        });
+        ticker.start();
+        window.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                System.out.println("Key pressed: " + ke.getKeyChar());
+                if (ke.getKeyChar() == 'm') {
+                    mode = mode ^ 1;
+                }
+            }
+        });
+    }
+
+    public void updateImage() {
+        double factor = 250.0 / heatMax;
+        switch (mode) {
+        case 0:
+            int me, mr, mw;
+            for (int i = 0; i < MSP430Core.MAX_MEM; i++) {
+                mw = me = mr = 0;
+                if (heatW[i] > 0) mw = 80;
+                if (heatR[i] > 0) mr = 80;
+                if (heatE[i] > 0) me = 80;
+                
+                heatmap.setRGB(i & 127, i / 128,
+                        ((int)(factor * heatW[i] + mw) << 16) |
+                        ((int)(factor * heatR[i] + mr) << 8) | 
+                        (int)(factor * heatE[i]) + me);
+            }
+            break;
+        case 1:
+            /* slow... */
+            for (int i = 0; i < MSP430Core.MAX_MEM; i++) {
+                int r = heatW[i];
+                int g = heatR[i];
+                int b = heatE[i];
+
+                heatW[i] = (99 * r) / 100;
+                heatR[i] = (99 * g) / 100;
+                heatE[i] = (99 * b) / 100;
+
+                if (r > 255) r = 255;
+                if (g > 255) g = 255;
+                if (b > 255) b = 255;
+                
+                heatmap.setRGB(i & 127, i / 128, (r << 16) | (g << 8) | b);
+            }
+            break;
+        }
+    }
+    
+    public void paint(Graphics g) {
+        updateImage();
+        g.drawImage(heatmap, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    @Override
+    public void cpuAction(int type, int adr, int data) {
+        int val = 0;
+        int f = 1;
+        if (mode == 1) f = 40;
+        switch (type) {
+        case CPUMonitor.EXECUTE:
+            val = heatE[adr] = heatE[adr] + f;
+            break;
+        case CPUMonitor.MEMORY_READ:
+            val = heatR[adr] = heatR[adr] + f;
+            break;
+        case CPUMonitor.MEMORY_WRITE:
+            val = heatW[adr] = heatW[adr] + f;
+            break;
+        }
+        if (val > heatMax) {
+            heatMax = val;
+        }
+    }
+    
+}
