c0391ecb1d8fb8d248080670cc599b691e947fa0
nifi <nifi@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2010-07-19 11:47:28 +0000

    Extended the ManagedWindow API
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@734 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

--- se/sics/mspsim/ui/JFrameWindowManager.java
+++ se/sics/mspsim/ui/JFrameWindowManager.java
@@ -10,18 +10,43 @@ public class JFrameWindowManager implements WindowManager {
         ManagedWindow w = new ManagedWindow() {
             private JFrame window = new JFrame(name);
             private boolean restored = false;
-            
-            public void add(Component component) {
-                if (!restored) {
-                    WindowUtils.restoreWindowBounds(name, window);
-                    WindowUtils.addSaveOnShutdown(name, window);
-                }
-                window.add(component);
-                window.pack();
+
+            public void setBounds(int x, int y, int width, int height) {
+                window.setBounds(x, y, width, height);
             }
+
+            public void add(Component component) {
+                window.add(component);
+                if (!restored) {
+                    restored = true;
+                    WindowUtils.restoreWindowBounds(name, window);
+                }
+            }
+
+            public void removeAll() {
+                window.removeAll();
+            }
+
+            public boolean isVisible() {
+                return window.isVisible();
+            }
+
             public void setVisible(boolean b) {
+                if (b != window.isVisible()) {
+                    if (b) {
+                        WindowUtils.addSaveOnShutdown(name, window);
+                    } else {
+                        WindowUtils.saveWindowBounds(name, window);
+                        WindowUtils.removeSaveOnShutdown(window);
+                    }
+                }
                 window.setVisible(b);
             }
+
+            public String getTitle() {
+                return window.getTitle();
+            }
+
             public void setTitle(String name) {
                 window.setTitle(name);
             }
--- se/sics/mspsim/ui/ManagedWindow.java
+++ se/sics/mspsim/ui/ManagedWindow.java
@@ -4,9 +4,16 @@ import java.awt.Component;
 
 public interface ManagedWindow {
 
+  public void setBounds(int x, int y, int width, int height);
+
   public void add(Component component);
+  public void removeAll();
+
+  public boolean isVisible();
 
   public void setVisible(boolean b);
+
+  public String getTitle();
 
   public void setTitle(String string);
 
