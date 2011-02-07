55acf74d94e98b91203944d63de55ee2b6ea3604
joxe <joxe@23d1a52b-0c3c-0410-b72d-8f29ab48fe35>
2011-02-07 10:53:43 +0000

    added missing file
    git-svn-id: https://mspsim.svn.sourceforge.net/svnroot/mspsim/mspsim@786 23d1a52b-0c3c-0410-b72d-8f29ab48fe35

---
+++ se/sics/mspsim/core/MSP430Config.java
@@ -0,0 +1,34 @@
+package se.sics.mspsim.core;
+
+public class MSP430Config {
+    
+    public class TimerConfig {
+        int ccr0Vector;
+        int ccrXVector;
+        int ccrCount;
+        int offset;
+        String name;
+        public int[] srcMap;
+        
+        public TimerConfig(int ccr0Vec, int ccrXVec, int ccrCount, int offset,
+                int[] srcMap, String name) {
+            ccr0Vector = ccr0Vec;
+            ccrXVector = ccrXVec;
+            this.ccrCount = ccrCount;
+            this.name = name;
+            this.offset = offset;
+            this.srcMap = srcMap;
+        }
+    }
+
+    /* default for the 149/1611 */
+    public TimerConfig[] timerConfig = {
+            new TimerConfig(6, 5, 3, 0x160, Timer.TIMER_Ax149, "TimerA"),
+            new TimerConfig(13, 12, 7, 0x180, Timer.TIMER_Bx149, "TimerB")
+    };
+    public int maxInterruptVector = 15;
+    public boolean MSP430XArch = false;
+    
+    public void setup() {        
+    }
+}
