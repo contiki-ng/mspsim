52996923022277b395f9470b5928d67febe3b193
Niclas Finne <nfi@sics.se>
2012-05-29 13:06:12 +0200

    Added node gui for the Zolertia Z1 platform

--- se/sics/mspsim/platform/z1/Z1Node.java
+++ se/sics/mspsim/platform/z1/Z1Node.java
@@ -1,9 +1,10 @@
 package se.sics.mspsim.platform.z1;
 
 import java.io.IOException;
-
+import se.sics.mspsim.chip.Button;
 import se.sics.mspsim.chip.CC2420;
 import se.sics.mspsim.chip.FileM25P80;
+import se.sics.mspsim.chip.Leds;
 import se.sics.mspsim.chip.M25P80;
 import se.sics.mspsim.config.MSP430f2617Config;
 import se.sics.mspsim.core.EmulationException;
@@ -19,6 +20,15 @@ import se.sics.mspsim.util.ArgumentManager;
 
 public class Z1Node extends GenericNode implements PortListener, USARTListener {
 
+    public static final int MODE_LEDS_OFF = 0;
+    public static final int MODE_LEDS_1 = 1;
+    public static final int MODE_LEDS_2 = 2;
+    public static final int MODE_LEDS_3 = 3;
+    public static final int MODE_MAX = MODE_LEDS_3;
+
+    // Port 2.5 - Button
+    public static final int BUTTON_PIN = 5;
+
     /* P1.2 - Input: FIFOP from CC2420 */
     /* P1.3 - Input: FIFO from CC2420 */
     /* P1.4 - Input: CCA from CC2420 */
@@ -33,23 +43,49 @@ public class Z1Node extends GenericNode implements PortListener, USARTListener {
     public static final int CC2420_VREG = (1 << 5);
     public static final int CC2420_CHIP_SELECT = 0x01;
 
+    public static final int TMP102_PWR_PIN = 0;
+    public static final int TMP102_PWR = 1 << TMP102_PWR_PIN;
 
-    IOPort port1;
-    IOPort port3;
-    IOPort port4;
-    IOPort port5;
+    public static final int I2C_DATA_PIN = 1;
+    public static final int I2C_DATA = 1 << I2C_DATA_PIN;
+    public static final int I2C_CLK_PIN = 2;
+    public static final int I2C_CLK = 1 << I2C_CLK_PIN;
+
+    private IOPort port1;
+    private IOPort port2;
+    private IOPort port3;
+    private IOPort port4;
+    private IOPort port5;
 
     public static final int LEDS_CONF_RED    = 0x10;
     public static final int LEDS_CONF_GREEN  = 0x40;
-    public static final int LEDS_CONF_YELLOW = 0x20;
+    public static final int LEDS_CONF_BLUE   = 0x20;
+    private static final int[] LEDS = { 0xff2020, 0x40ff40, 0x4040ff };
 
+    boolean redLed;
+    boolean blueLed;
+    boolean greenLed;
+
+    private Leds leds;
+    private Button button;
+    private Z1Gui gui;
+
+    private CC2420 radio;
+//    private TMP102 tmp102;
     private M25P80 flash;
     private String flashFile;
-    public CC2420 radio;
-
 
     public Z1Node() {
         super("Z1", new MSP430f2617Config());
+        setMode(MODE_LEDS_OFF);
+    }
+
+    public Leds getLeds() {
+        return leds;
+    }
+
+    public Button getButton() {
+        return button;
     }
 
     public M25P80 getFlash() {
@@ -71,20 +107,37 @@ public class Z1Node extends GenericNode implements PortListener, USARTListener {
         }
     }
 
+    private int lastPort5 = 0;
     public void portWrite(IOPort source, int data) {
-        if (source == port5) {
-            System.out.println("LEDS GREEN = " + ((data & LEDS_CONF_GREEN) > 0));
-            System.out.println("LEDS RED = " + ((data & LEDS_CONF_RED) > 0));
-            System.out.println("LEDS YELLOW = " + ((data & LEDS_CONF_YELLOW) > 0));
-        }
-        if (source == port3) {
+        switch (source.getPort()) {
+        case 3:
             // Chip select = active low...
             radio.setChipSelect((data & CC2420_CHIP_SELECT) == 0);
-        }
-        if (source == port4) {
+            break;
+        case 4:
             radio.setVRegOn((data & CC2420_VREG) != 0);
             //radio.portWrite(source, data);
             flash.portWrite(source, data);
+            break;
+        case 5: {
+            if ((data & (LEDS_CONF_RED|LEDS_CONF_BLUE|LEDS_CONF_GREEN)) !=
+                (lastPort5 & (LEDS_CONF_RED|LEDS_CONF_BLUE|LEDS_CONF_GREEN))) {
+                redLed = (data & LEDS_CONF_RED) == 0;
+                blueLed = (data & LEDS_CONF_BLUE) == 0;
+                greenLed = (data & LEDS_CONF_GREEN) == 0;
+                leds.setLeds((redLed ? 1 : 0) + (greenLed ? 2 : 0) + (blueLed ? 4 : 0));
+                int newMode = (redLed ? 1 : 0) + (greenLed ? 1 : 0) + (blueLed ? 1 : 0);
+                setMode(newMode);
+            }
+            if ((data & TMP102_PWR) != (lastPort5 & TMP102_PWR)) {
+//                tmp102.setPowerPin((data & TMP102_PWR) != 0);
+            }
+            if ((data & (I2C_CLK | I2C_DATA)) != (lastPort5 & (I2C_CLK|I2C_DATA))) {
+//                tmp102.setI2CPins((data & I2C_CLK) != 0, (data & I2C_DATA) != 0);
+            }
+            lastPort5 = data;
+            break;
+        }
         }
     }
 
@@ -94,7 +147,9 @@ public class Z1Node extends GenericNode implements PortListener, USARTListener {
         }
 
         port1 = cpu.getIOUnit(IOPort.class, "P1");
-        port1.addPortListener(this);
+//        port1.addPortListener(this);
+        port2 = cpu.getIOUnit(IOPort.class, "P2");
+//        port2.addPortListener(this);
         port3 = cpu.getIOUnit(IOPort.class, "P3");
         port3.addPortListener(this);
         port4 = cpu.getIOUnit(IOPort.class, "P4");
@@ -102,18 +157,23 @@ public class Z1Node extends GenericNode implements PortListener, USARTListener {
         port5 = cpu.getIOUnit(IOPort.class, "P5");
         port5.addPortListener(this);
 
-        IOUnit usart0 = cpu.getIOUnit("USCI B0");
-        if (usart0 instanceof USCI) {
+//        tmp102 = new TMP102(cpu);
+
+        USCI usart0 = cpu.getIOUnit(USCI.class, "USCI B0");
+        if (usart0 != null) {
             radio = new CC2420(cpu);
             radio.setCCAPort(port1, CC2420_CCA);
             radio.setFIFOPPort(port1, CC2420_FIFOP);
             radio.setFIFOPort(port1, CC2420_FIFO);
 
-            ((USARTSource) usart0).addUSARTListener(this);
+            usart0.addUSARTListener(this);
             radio.setSFDPort(port4, CC2420_SFD);
         } else {
             throw new EmulationException("Could not setup mote - missing USCI B0");
         }
+
+        leds = new Leds(cpu, LEDS);
+        button = new Button("Button", cpu, port2, BUTTON_PIN, true);
     }
 
     public void setupNode() {
@@ -151,11 +211,14 @@ public class Z1Node extends GenericNode implements PortListener, USARTListener {
     }
 
     public void setupGUI() {
-        System.out.println("No gui for Z1 yet...");
+        if (gui == null) {
+            gui = new Z1Gui(this);
+            registry.registerComponent("nodegui", gui);
+        }
     }
 
     public int getModeMax() {
-        return 0;
+        return MODE_MAX;
     }
 
     public static void main(String[] args) throws IOException {
